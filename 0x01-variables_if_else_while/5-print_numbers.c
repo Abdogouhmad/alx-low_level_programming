9ccd0d9849f5796e3748124828570d87660e5e7a
musashi <gouhmada@gmail.com>
2023-03-17 17:20:59 +0100

    new

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+	printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
