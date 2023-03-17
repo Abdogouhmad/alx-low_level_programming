5f900beff19db51017bf134550d570b5e07c1ce5
musashi <gouhmada@gmail.com>
2023-03-17 16:50:53 +0100

    added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+		putchar (c);
+		}
+		c++;
+	}
+	putchar ('\n');
+	return (0);
+}
