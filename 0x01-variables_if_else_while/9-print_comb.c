66512ac05015d395180057bf1b6d1322e57f4677
musashi <gouhmada@gmail.com>
2023-03-17 20:44:45 +0100

    new

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar (',');
+			putchar (' ');
+		}
+		i++;
+	}
+	putchar ('\n');
+	return (0);
+}
