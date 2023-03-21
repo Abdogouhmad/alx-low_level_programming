40cadbcad5f2ae0747b4b8cd4c09e6ba41e25de4
musashi <gouhmada@gmail.com>
2023-03-21 17:40:51 +0000

    putchar

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * _islower - Checks for lowercase character
+  *
+  * Return: 1 for lowercase character or 0 for anything else
+*/
+
+int _islower(int c)
+{
+	if (c == 99)
+	{
+		return (1);
+	}
+	return (0);
+
+}
