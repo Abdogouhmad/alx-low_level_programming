4253eac6c181a3f0ec5919e636cc5a7e3cd8b042
musashi <gouhmada@gmail.com>
2023-04-03 16:19:34 +0000

    added

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _memset -fill the memory 
+ * @s: will be the filler to n
+ * @n: is to be filledwith s
+ * @b: is the constant 
+ * Return: the S
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+    memset(s, b, n);
+
+    return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,9 @@
+# welcome to more advanced arrays and pointers 
+## task 0 
+## task 1
+## task 2
+## task 3
+## task 4
+## task 5 
+## task 6
+## task 7
