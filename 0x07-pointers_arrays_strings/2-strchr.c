1ff96d01f0b6a58cb6fe5a1215159ccac94969e7
musashi <gouhmada@gmail.com>
2023-04-03 18:03:49 +0000

    added

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strchr -gets the length of a prefix substring.
+ * @s: is the string that we would like to count
+ * @c: consists entirely of bytes
+ * Return: the number of bytes that s holds
+*/
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; *s != '\0'; i++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
