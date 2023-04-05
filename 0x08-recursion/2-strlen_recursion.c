d08054f52f57941148aa93813c1bec8760503c51
musashi <gouhmada@gmail.com>
2023-04-05 16:53:06 +0000

    added

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen_recursion -get the string len
+ * @s: is the target string
+ * if the @s == to 0 print 0
+ * else print the len of string
+ * Return: the value of the len
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
