e998a9fbc955ff500515ca7db93a787908580b63
musashi <gouhmada@gmail.com>
2023-04-05 16:08:38 +0000

    added

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion -string recursion
+ * @s: recusing the string through if base condition
+ * Return: void
+*/
+void _puts_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	 putchar (*s);
+	 _puts_recursion(s + 1);
+
+
+}
