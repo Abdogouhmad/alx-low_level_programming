e91a36cd1c421e85fcb5b045e8391009620980a4
musashi <gouhmada@gmail.com>
2023-04-05 18:19:04 +0000

    added

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,12 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * 
- * 
- * 
- * 
+ * _pow_recursion -factorial
+ * @x: 1st number
+ * @y: 2nd number
+ * Return: the value of pow
 */
 int _pow_recursion(int x, int y)
 {
-    
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y  - 1));
 }
