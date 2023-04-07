1e8ea026ee44cc15e9f5f99d85c163eb21f062ec
musashi <gouhmada@gmail.com>
2023-04-07 14:30:52 +0000

    added

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# by div_styl
-#the next command will gather all the c files and compiled them at one time:
-gcc -Wall -pedantic -Werror -Wextra -c *.c 
-#after that put them inside the lib with this command: 
+gcc -Wall -pedantic -Werror -Wextra -c *.c
 ar -rc liball.a *o
-#create index with this cmd:
 ranlib liball.a
