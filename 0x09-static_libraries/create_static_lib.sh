bec029d91caae80a8041440af8dc08a556ad04a0
musashi <gouhmada@gmail.com>
2023-04-07 14:19:14 +0000

    added

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# by div_styl
+#the next command will gather all the c files and compiled them at one time:
+gcc -Wall -pedantic -Werror -Wextra -c *.c 
+#after that put them inside the lib with this command: 
+ar -rc liball.a *o
