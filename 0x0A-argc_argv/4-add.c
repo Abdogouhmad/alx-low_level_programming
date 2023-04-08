e42acd67f10f599dac1bda2067283a1916d19e77
musashi <gouhmada@gmail.com>
2023-04-08 11:34:47 +0000

    added

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/*by div-styl*/
+/**
+ * main - add up two numbers.
+ * incase you gave character or odd symble the output: Error and
+ * Return 1
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char  **argv)
+{
+	int a, b, sum;
+
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b] != '\0'; b++)
+		{
+			if (!isdigit(argv[a][b]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum = atoi(argv[1]);
+		sum += atoi(argv[a]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
