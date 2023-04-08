6d58aa57c6b7cae52cdfe2ed3912f087d789e6d0
musashi <gouhmada@gmail.com>
2023-04-08 02:08:19 +0000

    added

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,8 +6,8 @@
  * @argv: unused array of argument
  * Return: 0
 */
-int main(int argc, char __attribute__((__unused__)) **argv)
+int main(int argc, char __attribute__((__unused__))**argv)
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
