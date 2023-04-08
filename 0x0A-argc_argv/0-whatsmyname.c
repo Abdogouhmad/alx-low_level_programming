159f0a0c3e5c236c8978cbd0763ad1efcaffd24a
musashi <gouhmada@gmail.com>
2023-04-08 01:41:38 +0000

    added

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+
+int main(int __attribute__((__unused__)) argc, char  **argv)
+{
+    printf("%s\n", argv[0]);
+    return(0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+# welcome to the argv and argc
