c3edccf8b3b1cc4a8510b3f301039d569278fd04
musashi <gouhmada@gmail.com>
2023-04-08 11:47:50 +0000

    added

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,6 +14,11 @@ int main(int argc, char  **argv)
 {
 	int a, b, sum;
 
+	if (argc < 2)
+	{
+		printf("0\n");
+	return (0);
+	}
 	for (a = 1; a < argc; a++)
 	{
 		for (b = 0; argv[a][b] != '\0'; b++)
