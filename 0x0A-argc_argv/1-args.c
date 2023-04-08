ad56f0f937d156fa8e86d2945bf55ea2a61d2d33
musashi <gouhmada@gmail.com>
2023-04-08 02:00:20 +0000

    added

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*by div-styl*/
+/**
+ * main - print the number of argument in the program
+ * @argc: the number of the argument willing to be printed
+ * @argv: unused array of argument
+ * Return: 0
+*/
+int main(int argc, char __attribute__((__unused__)) **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
