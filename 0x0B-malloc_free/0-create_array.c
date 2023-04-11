1a3dd818c874d29aaa4830e98f671ea3bcd1a88f
musashi <gouhmada@gmail.com>
2023-04-11 13:45:28 +0000

    updated

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,12 +11,13 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
+	char *matrix;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	char *matrix = (char *) malloc(size * sizeof(char *));
+	matrix = (char *) malloc(size * sizeof(char *));
 
 	if (matrix == NULL)
 	{
