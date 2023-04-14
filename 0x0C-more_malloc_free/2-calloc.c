22209c1a5f67bafe6be5961aa93bf7e143f8cf9f
musashi <gouhmada@gmail.com>
2023-04-14 00:32:13 +0000

    task2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib>
+/*by div-styl*/
+/**
+ *memset -set black of memory to value
+ *@ptr: 1st argu
+ *@v: 2nd value
+ *@nb: size
+ *Return: ptr
+*/
+void *memset(void *ptr, int v, size_t nb)
+{
+	unsigned char *p = (unsigned char *)ptr;
+
+	for (size_t i = 0; i < num; i++)
+	{
+		p[i] = (unsigned char)value;
+	}
+	return (ptr);
+}
+/**
+ *_calloc - function that allocates memory for an array, using malloc.
+ *@nmemb: 1st argu
+ *@size: 2nd agru and value
+ *Return: calloc
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *cal;
+
+	if (nmemb == NULL)
+		return (NULL);
+	if (size == NULL)
+		return (NULL);
+	size_t sum = nmemb * size;
+
+	cal = malloc(sum);
+	if (cal == NULL)
+		return (NULL);
+	memset(cal, 0, sum);
+	return (cal);
+}
