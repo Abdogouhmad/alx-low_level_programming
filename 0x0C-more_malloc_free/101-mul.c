5135d5a6ae6399b3b5834d86fa936ecf27de8987
musashi <gouhmada@gmail.com>
2023-04-14 20:14:22 +0000

    last

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,16 +3,18 @@
 #include <ctype.h>
 /*by div-styl*/
 /**
- *multy -mutlyplay the agruments
- *@num1: the 1st argu
- *@num2: the 2nd argu
- *Return: the sum
+ * multiply - multiply the arguments
+ * @num1: the 1st argument
+ * @num2: the 2nd argument
+ * Return: the sum
  */
-int multy(char *num1, char *num2)
+int multiply(char *num1, char *num2)
 {
-	int sum = 0, i = 0;
+	int sum = 0;
+	int i = 0;
 
-	for (i; num1[i] != '\0')
+	// Check if num1 contains only digits
+	for (i = 0; num1[i] != '\0'; i++)
 	{
 		if (!isdigit(num1[i]))
 		{
@@ -20,7 +22,9 @@ int multy(char *num1, char *num2)
 			exit(98);
 		}
 	}
-	for (i; num2[i]; i++)
+
+	// Check if num2 contains only digits
+	for (i = 0; num2[i]; i++)
 	{
 		if (!isdigit(num2[i]))
 		{
@@ -28,36 +32,38 @@ int multy(char *num1, char *num2)
 			exit(98);
 		}
 	}
+
 	sum = atoi(num1) * atoi(num2);
 	return (sum);
 }
 
 /**
- *main -get the argu from user
- *@argc: numb of argu
- *@argv: the argu
- *Return: the total
-*/
-
-
-int main(int argc, char argv[])
+ * main - get the arguments from user
+ * @argc: number of arguments
+ * @argv: the arguments
+ * Return: the total
+ */
+int main(int argc, char *argv[])
 {
-	int nm1, nm2, total = 0;
+	int num1, num2, total = 0;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	nm1 = atoi(argv[1]);
-	nm2 = atoi(argv[2]);
 
-	if (nm1 == 0 || nm2 == 0)
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	if (num1 <= 0 || num2 <= 0)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	sum = multy(argv[1], argv[2]);
-	printf("%d\n", sum);
+
+	total = multiply(argv[1], argv[2]);
+	printf("%d\n", total);
+
 	return (0);
 }
