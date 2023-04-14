89cc9f8bf77843e928995789df64c2b3432f2d28
musashi <gouhmada@gmail.com>
2023-04-14 02:02:55 +0000

    task4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/*by div-styl*/
+/**
+ *_realloc -function that reallocates a memory block using malloc and free
+ *@ptr: pointer
+ *@old_size: 1st int
+ *@new_size: 2nd int
+ *Return: string
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i;
+	void *m;
+	char *srt = (char *)ptr;
+	char *dest = (char *)m;
+
+	if (ptr == NULL)
+	:	return (malloc(new_size));
+	if (new_size == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	m = malloc(new_size);
+	for (i = 0; i < old_size && i < new_size; i++)
+	{
+		dest[i] = srt[i];
+	}
+
+	free(ptr);
+	return (m);
+}
