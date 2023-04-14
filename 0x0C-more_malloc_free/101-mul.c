41ac153cd0d4207ea02f444b84eba4b66b1eade1
musashi <gouhmada@gmail.com>
2023-04-14 23:41:09 +0000

    updated

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,69 +1,65 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include "main.h"
-/*by div-styl*/
-/**
- * multiply - multiply the arguments
- * @num1: the 1st argument
- * @num2: the 2nd argument
- * Return: the sum
- */
-int multiply(char *num1, char *num2)
-{
-	int sum = 0;
-	int i = 0;
-
-	for (i = 0; num1[i] != '\0'; i++)
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include "main.h"
+	/*by div-styl*/
+	/**
+	 * is_digit -check if the character is num
+	 * @str: is a string or the argu
+	 * Return: 1
+	*/
+	int is_digits(char *str)
 	{
-		if (!isdigit(num1[i]))
+		int i;
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			if (str[i] < '0' || str[i] > '9')
+				return 0;
+		}
+		return 1;
+	}
+
+	/**
+	 * multiply - multiply the arguments
+	 * @num1: the 1st argument
+	 * @num2: the 2nd argument
+	 * Return: the sum
+	 */
+	int multiply(char *num1, char *num2)
+	{
+		int sum = 0;
+		sum = atoi(num1) * atoi(num2);
+		return sum;
+	}
+
+	/**
+	 * main - get the arguments from user
+	 * @argc: number of arguments
+	 * @argv: the arguments
+	 * Return: the total
+	 */
+	int main(int argc, char *argv[])
+	{
+		char *num1;
+		char *num2;
+		int total = 0;
+
+		if (argc != 3 || !is_digits(argv[1]) || !is_digits(argv[2]))
 		{
 			printf("Error\n");
 			exit(98);
 		}
-	}
 
-	for (i = 0; num2[i]; i++)
-	{
-		if (!isdigit(num2[i]))
+		num1 = argv[1];
+		num2 = argv[2];
+
+		if (atoi(num1) <= 0 || atoi(num2) <= 0)
 		{
 			printf("Error\n");
 			exit(98);
 		}
+
+		total = multiply(num1, num2);
+		printf("%d\n", total);
+
+		return (0);
 	}
-
-	sum = atoi(num1) * atoi(num2);
-	return (sum);
-}
-/**
- * main - get the arguments from user
- * @argc: number of arguments
- * @argv: the arguments
- * Return: the total
- */
-int main(int argc, char *argv[])
-{
-	char *num1;
-	char *num2;
-	int total = 0;
-
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	num1 = argv[1];
-	num2 = argv[2];
-
-	if (atoi(num1) <= 0 || atoi(num2) <= 0)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	total = multiply(num1, num2);
-	printf("%d\n", total);
-
-	return (0);
-}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 void *malloc_checked(unsigned int b);
+int _putchar(char c);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
