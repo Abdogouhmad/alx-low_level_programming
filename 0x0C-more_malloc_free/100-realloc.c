54d2a5a1621332ae210183b32815b6af6c56dc16
musashi <gouhmada@gmail.com>
2023-04-14 02:05:24 +0000

    task4fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,13 +16,17 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *dest = (char *)m;
 
 	if (ptr == NULL)
-	:	return (malloc(new_size));
-	if (new_size == NULL)
+		return (malloc(new_size));
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
+	if (new_size == old_size)
+		return (ptr);
+
 	m = malloc(new_size);
+
 	for (i = 0; i < old_size && i < new_size; i++)
 	{
 		dest[i] = srt[i];
