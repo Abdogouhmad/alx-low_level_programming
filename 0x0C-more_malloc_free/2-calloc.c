d4922a4c3051942fac94fe216f7a115f4b4017db
musashi <gouhmada@gmail.com>
2023-04-14 00:51:52 +0000

    task2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,42 +1,51 @@
 #include "main.h"
-#include <stdlib>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 /*by div-styl*/
 /**
- *_memset -set black of memory to value
- *@ptr: 1st argu
- *@v: 2nd value
- *@nb: size
- *Return: ptr
-*/
+ * _memset - sets block of memory to a value
+ * @ptr: pointer to the memory block
+ * @v: value to set
+ * @nb: size of the memory block
+ *
+ * Return: ptr
+ */
 void *_memset(void *ptr, int v, size_t nb)
 {
 	unsigned char *p = (unsigned char *)ptr;
+	size_t i = 0;
 
-	for (size_t i = 0; i < num; i++)
+	for (i = 0; i < nb; i++)
 	{
-		p[i] = (unsigned char)value;
+		p[i] = (unsigned char)v;
 	}
 	return (ptr);
 }
+
 /**
- *_calloc - function that allocates memory for an array, using malloc.
- *@nmemb: 1st argu
- *@size: 2nd agru and value
- *Return: calloc
+ * _calloc - function that allocates memory for an array, using malloc.
+ * @nmemb: number of elements
+ * @size: size of each element
+ *
+ * Return: pointer to the allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	size_t sum = 0;
 	void *cal;
 
-	if (nmemb == NULL)
+	if (nmemb == 0)
 		return (NULL);
-	if (size == NULL)
+	if (size == 0)
 		return (NULL);
-	size_t sum = nmemb * size;
+
+	sum = nmemb * size;
 
 	cal = malloc(sum);
 	if (cal == NULL)
 		return (NULL);
+
 	_memset(cal, 0, sum);
 	return (cal);
 }
