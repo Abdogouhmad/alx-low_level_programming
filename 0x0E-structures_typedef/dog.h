70ec73dbb59ad9ded9600ef4b752d91fd82a5a60
musashi <gouhmada@gmail.com>
2023-04-17 17:34:28 +0000

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog -name of dog and age and owner
+ *@name: name of dog
+ *@age: the age of dog
+ *@owner: of the dog
+ *Description: long Description
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+
+#endif
