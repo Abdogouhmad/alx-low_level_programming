83d9cae59e8b0f77ec450aaf030f08618f563abe
musashi <gouhmada@gmail.com>
2023-04-17 18:24:18 +0000

    updated

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		d->name = "(nil)";
-	else if (d->owner == NULL)
+	if (d->owner == NULL)
 		d->owner = "(nil)";
 printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
