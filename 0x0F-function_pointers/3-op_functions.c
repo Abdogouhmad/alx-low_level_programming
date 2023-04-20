e89dbe7e120d3f75bff4e134b7c3f51cba90429e
musashi <gouhmada@gmail.com>
2023-04-20 02:50:53 +0000

    updated

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,46 +1,42 @@
 #include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
 
 /**
- * op_add - Returns the add
- * @a: The first number.
- * @b: The second number.
- *
- * Return: a and b
+ * op_add - sum of a and b
+ * @a: integer to add
+ * @b: integer to add
+ * Return: sum if succesful
  */
+
 int op_add(int a, int b)
 {
-	return (a + b);
+	return ((a) + (b));
 }
 /**
- * op_sub - Returns the sub
- * @a: The first number.
- * @b: The second number.
- *
- * Return: a and b
+ * op_sub - difference of a and b
+ * @a: integer to subtract
+ * @b: integer to subtract
+ * Return: difference if succesful
  */
 int op_sub(int a, int b)
 {
-	return (a - b);
+	return ((a) - (b));
 }
 /**
- * op_mul - Returns the multy
- * @a: The first number.
- * @b: The second number.
- *
- * Return: a and b
+ * op_mul - multiply a and b
+ * @a: integer to multiply
+ * @b: integer to multiply
+ * Return: multiplication of a and b if succesful
  */
 int op_mul(int a, int b)
 {
-	return (a * b);
+	return ((a) * (b));
 }
 /**
- * op_div - Returns the divi
- * @a: The first number.
- * @b: The second number.
- *
- * Return: a and b
+ * op_div - divides a and b
+ * @a: integer to divide
+ * @b: integer to divide
+ * Return: division if succesful
  */
 int op_div(int a, int b)
 {
@@ -52,11 +48,10 @@ int op_div(int a, int b)
 	return ((a) / (b));
 }
 /**
- * op_mod - Returns the modulo
- * @a: The first number.
- * @b: The second number.
- *
- * Return: a and b
+ * op_mod - Module of a and b
+ * @a: integer
+ * @b: integer
+ * Return: remainder if succesful
  */
 int op_mod(int a, int b)
 {
