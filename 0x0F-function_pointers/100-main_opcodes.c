57d6359c776186eb2f26f79e831a626ea63afae8
musashi <gouhmada@gmail.com>
2023-04-20 00:26:14 +0000

    advfixed

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -15,34 +15,34 @@ int bit, index;
 char *matrix;
 
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        exit(1);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
 
-    bit = atoi(argv[1]);
+	bit = atoi(argv[1]);
 
 
-    if (bit < 0)
-    {
-        printf("Error\n");
-        exit(2);
-    }
+	if (bit < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
 
-    matrix = (char *)main;
+	matrix = (char *)main;
 
 
-    for (index = 0; index < bit; index++)
-    {
-        if (index == bit - 1)
-        {
-            printf("%02hhx\n", matrix[index]);
-            break;
-        }
-        printf("%02hhx ", matrix[index]);
-    }
+	for (index = 0; index < bit; index++)
+	{
+		if (index == bit - 1)
+		{
+			printf("%02hhx\n", matrix[index]);
+			break;
+		}
+		printf("%02hhx ", matrix[index]);
+	}
 return (0);
 }
