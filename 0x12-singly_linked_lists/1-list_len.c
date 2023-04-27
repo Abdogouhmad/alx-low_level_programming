eed29dab8b6e262137acc8a2cf9f39bade7a6b39
musashi <gouhmada@gmail.com>
2023-04-27 19:06:24 +0100

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * list_len - returns the number of elements in a 
+ * linked list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in a linked list_t list.
+*/
+/*BY DIV-STYL*/
+size_t list_len(const list_t *h)
+{
+	size_t linked_list_len = 0;
+
+	while (h != NULL)
+	{
+		linked_list_len += 1;
+		h = h->next;
+	}
+	return (linked_list_len);
+}
