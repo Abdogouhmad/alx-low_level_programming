55895ee988e973ce765095cb1ca8cd96e8f9bf29
musashi <gouhmada@gmail.com>
2023-04-27 19:07:19 +0100

    betty task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * list_len - returns the number of elements in a 
+ * list_len - returns the number of elements in a
  * linked list_t list.
  * @h: singly linked list.
  * Return: number of elements in a linked list_t list.
