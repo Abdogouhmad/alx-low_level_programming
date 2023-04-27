b31916a4f04ba511d6a9bb17aca7214967e1e385
musashi <gouhmada@gmail.com>
2023-04-28 00:01:30 +0100

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/* by div-styl*/
+/**
+ * print_into_theconsol - prints a string into the consol
+ * Return: void
+*/
+void print_into_theconsol(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_into_theconsol();
+    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+    return (0);
+}
