ec83739c6a2ec0d9b6d62464c6e4fca3fb306774
musashi <gouhmada@gmail.com>
2023-04-28 00:06:49 +0100

    update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -29,4 +29,5 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 void print_into_theconsol(void);
+print_into_theconsol(void)__attribute__((constructor));
 #endif
