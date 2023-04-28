70914bc625ca7ac50a892fba4fcadfb794300c0e
musashi <gouhmada@gmail.com>
2023-04-28 01:04:34 +0100

    last

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,12 @@
+section .data
+    msg db 'Hello, Holberton', 10, 0
+
+section .text
+    global main
+    extern printf
+main:
+    mov edi , msg
+    xor eax, eax
+    call printf
+    xor eax, eax
+    ret
