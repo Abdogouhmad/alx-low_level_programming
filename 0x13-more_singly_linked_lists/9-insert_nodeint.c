49f09e986e2ef36d06b83902505f5ae283bfebfa
musashi <gouhmada@gmail.com>
2023-04-30 22:59:53 +0100

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,40 +12,24 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *newNode, *tempo = *head;
 	unsigned int i = 0;
 
-	if  (head == NULL)
-	{
-		if (idx != 0)
-		{
-			*head = malloc(sizeof(listint_t));
-			if  (*head == NULL)
-				return  (NULL);
-			(*head)->n = n;
-			(*head)->next = NULL;
-			return (*head);
-		}
-			return (NULL);
-	}
-	if (idx == 0)
-	{
-		tempo = malloc(sizeof(listint_t));
-		if (tempo == NULL)
-			return (NULL);
-		newNode->n = n;
-		newNode->next = *head;
-		*head = newNode;
-		return (newNode);
-	}
-	while (tempo < idx - 1 && tempo->next != NULL)
-	{
-		tempo = tempo->next;
-		i++;
-	}
-	if (i < idx - 1)
-		return (NULL);
+	if (head == NULL || idx < 0)
+		return  (NULL);
 	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
 		return (NULL);
 	newNode->n = n;
+	if (idx == 0)
+	{
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
+	}
+	for  (i = 0; i < idx - 1; i++)
+	{
+		if (tempo == NULL)
+			return (NULL);
+		tempo  = tempo->next;
+	}
 	newNode->next = tempo->next;
 	tempo->next = newNode;
 	return (newNode);
