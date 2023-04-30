815c5a4a384bb0125963bfafadca28fba58d1e05
musashi <gouhmada@gmail.com>
2023-04-30 23:09:17 +0100

    task 9 upd

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,25 +10,20 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *newNode, *tempo = *head;
-	unsigned int i = 0;
+	unsigned int i;
 
-	if (head == NULL || idx < 0)
+	if (head == NULL || idx == 0)
 		return  (NULL);
 	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
 		return (NULL);
 	newNode->n = n;
-	if (idx == 0)
+	for (i = 0; i < idx - 1 && tempo != NULL; i++)
+		tempo = tempo->next;
+	if (tempo == NULL)
 	{
-		newNode->next = *head;
-		*head = newNode;
-		return (newNode);
-	}
-	for  (i = 0; i < idx - 1; i++)
-	{
-		if (tempo == NULL)
-			return (NULL);
-		tempo  = tempo->next;
+		free(newNode);
+		return  (NULL);
 	}
 	newNode->next = tempo->next;
 	tempo->next = newNode;
