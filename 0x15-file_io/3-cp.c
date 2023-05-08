e7f67b540ed5ac7bc37130aca402db9e18c7b78f
musashi <gouhmada@gmail.com>
2023-05-08 20:36:24 +0100

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,7 +30,7 @@ void close_file(int fd)
 	if (closefile == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exite(100);
+		exit(100);
 	}
 }
 /**
@@ -39,7 +39,7 @@ void close_file(int fd)
  * @argv: argument vector
  * Return: 0
 */
-int main(int argc, char **argv[])
+int main(int argc, char *argv[])
 {
 	int fd_from, fd_to, r, w;
 	char *buffer;
