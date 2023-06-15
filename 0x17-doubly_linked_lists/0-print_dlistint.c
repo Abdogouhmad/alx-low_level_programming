a3298ed3f437510b8c369ba3f68d954a934b695a
div-styl <gouhmada@gmail.com>
2023-06-15 13:57:47 +0100

    updated

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,6 +7,7 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t cnt = 0;
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
@@ -15,4 +16,3 @@ size_t print_dlistint(const dlistint_t *h)
 	}
 	return (cnt);
 }
-
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,7 +8,7 @@ size_t dlistint_len(const dlistint_t *h)
 {
 	size_t dlist_len = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
 		dlist_len += 1;
 		h = h->next;
