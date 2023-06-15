492a657e54e468b7f89989ab35a4d55982e1463c
div-styl <gouhmada@gmail.com>
2023-06-15 15:03:35 +0100

    task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+* add_dnodeint - add nmber to dlist 
+* @head: the head lists
+* @n: the number to be added
+* Return: the list
+*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *add_n = malloc(sizeof(dlistint_t));
+
+	if (add_n == NULL)
+	{
+		free(add_n);
+		return (NULL);
+	}
+
+	add_n->n = n;
+	add_n->prev = NULL;
+	add_n->next = *head;
+
+	if (*head != NULL)
+		(*head)->prev = add_n;
+
+	*head = add_n;
+	return (add_n);
+}
+
