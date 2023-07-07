6975dbf692e9d9d5cd1793302393a873d5aebed1
div-styl <gouhmada@gmail.com>
2023-07-07 11:50:12 +0100

    ok lemme try this

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -26,7 +26,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(current->key, key) == 0)
 		{
 			free(current->key);
-			current->key = strdup(value);
+			current->value = strdup(value);
 			return (1);
 		}
 		current = current->next;
