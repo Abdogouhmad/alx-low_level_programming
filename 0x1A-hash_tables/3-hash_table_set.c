22b41609ad6f1f5a34de90a7e67d5a7983bff8e8
div-styl <gouhmada@gmail.com>
2023-07-07 20:32:13 +0100

    free the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,6 +27,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(current->value);
 			current->value = strdup(value);
+      free(new_node);
 			return (1);
 		}
 		current = current->next;
